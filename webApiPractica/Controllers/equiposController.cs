ca570652e3c0da8f682eee3aa294fb321894e008
Gladys <Gladys@DESKTOP-D4GHUKT>
2025-02-04 17:05:54 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+        public equiposController(equiposContext equiposContext) { 
+            _equiposContext = equiposContext;
+        }
+
+        /// <summary>
+        /// ENdpoint que retorna el listado de todos los equipos existentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get() {
+            List<equipos> ListadoEquipo = (from e in _equiposContext.equipos select e).ToList();
+            if(ListadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(ListadoEquipo);
+        }
+    }
+}
---
+++ webApiPractica/Models/equipos.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webApiPractica.Models
+{
+    public class equipos
+    {
+        [Key]  // Define que esta es la clave primaria
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_equipo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public int tipo_equipo_id { get; set; }
+
+        public int marca_id { get; set; }
+
+        [StringLength(50)]
+        public string modelo { get; set; }
+
+        public int anio_compra { get; set; }
+
+        [Column(TypeName = "NUMERIC(18,4)")]
+        public decimal costo { get; set; }
+
+        public int vida_util { get; set; }
+
+        public int estado_equipo_id { get; set; }
+
+        [Required]
+        public char estado { get; set; }
+    }
+}
